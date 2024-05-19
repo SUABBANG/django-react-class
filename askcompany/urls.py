f32a70ce84e61d68634a193b703c7763f29a6901
suang <suabbang@gmail.com>
2024-05-19 17:59:40 +0900

    init basic front

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', TemplateView.as_view(template_name='root.html'), name='root.html')
+    re_path('', TemplateView.as_view(template_name='root.html'), name='root.html') # re_path : url 을 자동으로 여기로 리턴
 ]
 
 if settings.DEBUG: # 개발 환경 에서만 사용s
